79c160d13e872961eb7d058407d7f7254c0eb48d
Alvin <alvinrobertlee@gmail.com>
2018-03-23 17:51:50 -0700

    Added gitignore

--- catan/db/index.js
+++
@@ -1,6 +0,0 @@
-const pgp = require('pg-promise')();
-require('dotenv').config();
-console.log("Database_url: " + process.env.DATABASE_URL);
-const connection = pgp(process.env.DATABASE_URL);
-
-module.exports = connection;
