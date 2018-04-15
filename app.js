34095b31eacc2dd3308cf259ac7ca9aeaf63c66c
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2018-04-15 14:52:33 -0700

    register page added, needs functionality

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var tests = require('./routes/tests');
+var register = require('./routes/register');
 
 
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/tests', tests);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
