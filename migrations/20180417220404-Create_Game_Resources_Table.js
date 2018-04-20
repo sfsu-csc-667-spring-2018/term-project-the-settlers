4c157333bdd4bda4441a9100f7cb380da61ea312
Ryan <ryanjung93@gmail.com>
2018-04-19 22:18:05 -0700

    Fixed syntax error

--- migrations/20180417220404-Create_Game_Resources_Table.js
+++ migrations/20180417220404-Create_Game_Resources_Table.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable( 
+    return queryInterface.createTable(
       'Game_Resources',
       {
         id: {
@@ -13,7 +13,7 @@ module.exports = {
         vertex_id: {
           type: Sequelize.INTEGER,
           allowNull: false
-        }, 
+        },
         dice_number: {
           type: Sequelize.INTEGER,
           allowNull: false
@@ -21,15 +21,15 @@ module.exports = {
         resource: {
           type: Sequelize.STRING,
           allowNull: false
-        }
+        },
         game_id: {
           type: Sequelize.INTEGER,
           allowNull: false
-        }
+        },
         createdAt: {
           type: Sequelize.DATE,
           allowNull: false
-        }
+        },
         updatedAt: {
           type: Sequelize.DATE,
           allowNull: false
@@ -39,6 +39,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Game_Resources'); 
+    return queryInterface.dropTable('Game_Resources');
   }
 };
