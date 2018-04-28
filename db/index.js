9249b0dfacbcc3712451f8be10707d36bdfc941a
Jaimes Subroto <jaimes_subroto@yahoo.com>
2018-04-27 20:46:59 -0700

    Correctly displays the username in chat

--- db/index.js
+++ db/index.js
@@ -1,18 +1,6 @@
 const pgp = require('pg-promise')();
 console.log("process.env.DATABASE_URL:" + process.env.DATABASE_URL);
 const connection = pgp(process.env.DATABASE_URL);
-const cn = {
-    host: 'localhost', // 'localhost' is the default;
-    port: 5432, // 5432 is the default;
-    database: 'catan',
-    user: 'postgres',
-    password: '1234'
-};
-
-const db = pgp(cn);
-
-
 
 exports = module.exports;
 exports.users = require('./users/')(connection);
-
