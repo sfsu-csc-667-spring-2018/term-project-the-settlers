74b8761f332722073193f7e5e7258174d013ff37
Ryan <ryanjung93@gmail.com>
2018-05-08 14:32:14 -0700

    Add game id page

--- routes/game.js
+++ routes/game.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const authenticate = require('../authentication/authenticated');
+const db = require('../db');
 
 router.use(authenticate);
 
-/* GET home page. */
+/*
 router.get('/', function(req, res, next) {
     console.log('from game', req.user);
     res.render('game', { title: 'Express', user: req.user.username });
-});
+}
+*/
+
+router.post('/',function(request,response,next){
+  db.games.createGame()
+  .then( game => {
+                  response.status(302).send({game:game.id});
+                  console.log(game.id);
+
+        }
+    )
+  .catch(error => console.log('error creating game ' + error));
+})
+
+router.get('/:id',function(request,response,next){
+  response.render('game', { title: 'Express', user: request.user.username, gameId: request.params.id });
+})
 
 module.exports = router;
