6a014974c581c16fae4c1a3dd58e1119d135f497
John Roberts <jhroberts@linkedin.com>
2018-05-08 16:44:04 -0700

    Started connecting front end and back end
    * I reformatted a bunch of files
    * Fixed lobby chat (post to b/e, b/e emits)
        * TODO: Correct the message name
    * Refactored f/e scripts into scripts/*
    * POST /game for game creation and redirect to game page
    * GET /game/:id for game display
    * Insert tiles in random order on game creation

--- routes/game.js
+++ routes/game.js
@@ -1,13 +1,35 @@
-var express = require('express');
-var router = express.Router();
-const authenticate = require('../authentication/authenticated');
+const express = require("express");
+const router = express.Router();
+const authenticate = require("../authentication/authenticated");
+const db = require("../db");
 
 router.use(authenticate);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log('from game', req.user);
-    res.render('game', { title: 'Express', user: req.user.username });
+router.get("/", function(req, res, next) {
+  console.log("from game", req.user);
+  res.render("game", { title: "Express", user: req.user.username });
+});
+
+router.post("/", function(req, res, next) {
+  db.games
+    .createGame()
+    .then(({ id }) => {
+      res.redirect(`/game/${id}`);
+    })
+    .catch(error => console.log(error));
+});
+
+router.get("/:id", (request, response, next) => {
+  const { username, id: userId } = request.user;
+  const { id: gameId } = request.params;
+
+  db.games
+    .getGame(gameId)
+    .then(result =>
+      response.render("game", Object.assign({}, result, { username, userId }))
+    )
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
