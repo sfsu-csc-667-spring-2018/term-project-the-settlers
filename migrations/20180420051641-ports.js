b49754409b98c9aa4a4aa1804a36c22160932a36
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2018-05-16 13:18:15 -0700

    inserted ports into the database

--- migrations/20180420051641-ports.js
+++ migrations/20180420051641-ports.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable("ports", {
@@ -31,9 +32,26 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       }
+    })
+    //var allShips = ["3for1", "3for1", "3for1", "3for1", "wheat", "ore", "lumber", "brick", "sheep"];
+    .then(()=> {
+      return queryInterface.bulkInsert(
+        "ports",
+        [
+          {resources_in_required: 3, resources_out_required: 1, resource_in_type: "any", resource_out_type: "any"},
+          {resources_in_required: 3, resources_out_required: 1, resource_in_type: "any", resource_out_type: "any"},
+          {resources_in_required: 3, resources_out_required: 1, resource_in_type: "any", resource_out_type: "any"},
+          {resources_in_required: 3, resources_out_required: 1, resource_in_type: "any", resource_out_type: "any"},
+          {resources_in_required: 2, resources_out_required: 1, resource_in_type: "wheat", resource_out_type: "any"},
+          {resources_in_required: 2, resources_out_required: 1, resource_in_type: "ore", resource_out_type: "any"},
+          {resources_in_required: 2, resources_out_required: 1, resource_in_type: "lumber", resource_out_type: "any"},
+          {resources_in_required: 2, resources_out_required: 1, resource_in_type: "brick", resource_out_type: "any"},
+          {resources_in_required: 2, resources_out_required: 1, resource_in_type: "sheep", resource_out_type: "any"},
+        ]
+      );
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Ports");
+    return queryInterface.dropTable("ports");
   }
 };
