bbc72f79b0dc16d28a7470cf10fb3156d0ef8952
Ryan <ryanjung93@gmail.com>
2018-05-17 14:33:30 -0700

    Added total resources to playerInfo

--- db/games/index.js
+++ db/games/index.js
@@ -34,12 +34,13 @@ const insertGameTiles = db => ([game, tiles]) => {
   const queries = shuffle(allProbs).map((probability, index) => {
     let order = index >= desertPlacementOrder ? index+1 : index;
     return db.none(
-      "INSERT INTO game_tiles VALUES(${game_id}, ${tile_id}, ${probability}, ${order})",
+      "INSERT INTO game_tiles VALUES(${game_id}, ${tile_id}, ${probability}, ${order}, ${robber})",
       {
         game_id: game.id,
         tile_id: tiles[index].id,
         probability,
-        order
+        order,
+        robber: false
       }
     )
   });
@@ -52,13 +53,14 @@ const insertGameTiles = db => ([game, tiles]) => {
 
 const insertDesertTile = db => (gameId,order) =>{
   db.none(
-    "INSERT INTO game_tiles VALUES(${game_id}, ${tile_id}, ${probability}, ${order})"
+    "INSERT INTO game_tiles VALUES(${game_id}, ${tile_id}, ${probability}, ${order}, ${robber})"
     ,
     {
       game_id: gameId,
       tile_id: 1,
       probability: 0,
-      order
+      order,
+      robber: true
     }
   )
 }
@@ -79,6 +81,21 @@ const insertGameVertices = db => gameId => {
   );
 };
 
+const getPlayerInfo = db => (gameId) => {
+  return db.any('SELECT username, turn_order,card_count,resource_count FROM players'
+          +' INNER JOIN users ON users.id = players.user_id'
+          +' LEFT JOIN (SELECT COUNT(*) AS card_count,player_id'
+          +'             FROM dev_cards GROUP BY player_id) cards ON cards.player_id = players.id '
+          +' LEFT JOIN (SELECT SUM(count) AS resource_count, player_id'
+          + '           FROM player_resources GROUP BY player_id) resources'
+          +' ON resources.player_id = players.id'
+          +' WHERE game_id = $1' , [gameId] );
+};
+
+const getPlayerResources = db => (gameId) => {
+  return db.any('SELECT SUM(count) FROM player_resources GROUP BY player_id WHERE game_id = $1,')
+};
+
 module.exports = db => {
   const gameFunctions = {};
 
@@ -87,14 +104,6 @@ module.exports = db => {
       .then(insertGameTiles(db))
       .then(insertGameEdges(db))
       .then(result => result[1]);
-
-  gameFunctions.getPlayerInfo = (gameId) => {
-    return db.any('SELECT username, turn_order,card_count FROM players'
-            +' INNER JOIN users ON users.id = players.user_id'
-            +' LEFT JOIN (SELECT COUNT(*) AS card_count,player_id'
-            +'             FROM dev_cards GROUP BY player_id) cards ON cards.player_id = players.id '
-            +' WHERE game_id = $1' , [gameId] );
-  }
 
   gameFunctions.getGame = id =>
     Promise.all([
@@ -105,7 +114,8 @@ module.exports = db => {
       ),
       db.many("SELECT * FROM game_vertices WHERE game_id=$1", [id]),
       db.many("SELECT * FROM game_edges WHERE game_id=$1", [id]),
-      gameFunctions.getPlayerInfo(id)
+      getPlayerInfo(db)(id),
+
     ]).then(([game, tiles, vertices, edges,playerInfo]) => ({
       game,
       tiles,
@@ -147,77 +157,25 @@ module.exports = db => {
     );
   };
 
-  gameFunctions.addPortToVertex = (
-    vertexNumber,
-    gameId,
-    requiredResourceType
-  ) => {
-    return db.tx("addPortTransaction", t =>
-      t
-        .one(
-          'SELECT id FROM "ports" WHERE UPPER(resource_in_type) = UPPER($1)',
-          [requiredResourceType]
-        )
-        .then(port => {
-          if (port) {
-            return t.none(
-              'UPDATE "vertices" set port_id = $1' +
-                "WHERE vertex_number = $2 " +
-                "AND game_id = $3",
-              [port.id, vertexNumber, gameId]
-            );
-          }
-          return [];
-        })
-    );
-  };
 
-  gameFunctions.moveRobber = (vertexNumber, gameId) => {
+  gameFunctions.moveRobber = (gameTileOrder, gameId) => {
     return db.tx("moveRobberTransaction", t => {
       t
-        .none('UPDATE "vertices" SET robber = $1 ' + "WHERE game_id = $2", [
+        .none('UPDATE "game_tiles" SET robber = $1 ' + "WHERE game_id = $2", [
           false,
           gameId
         ])
         .then(() =>
           t
             .none(
-              'UPDATE "vertices" SET robber = $1 ' +
-                "WHERE vertex_number = $2 AND game_id = $3",
-              [true, vertexNumber, gameId]
+              'UPDATE "game_tiles" SET robber = $1 ' +
+                "WHERE order = $2 AND game_id = $3",
+              [true, gameTileOrder, gameId]
             )
             .catch(error => error)
         );
     });
   };
 
-  gameFunctions.findVertexId = (vertexNumber, game) => {
-    return db.one(
-      'SELECT id FROM "vertices"' +
-        " WHERE vertex_number = $1 AND game_id = $2 ",
-      [vertexNumber, gameId]
-    );
-  };
-
-  gameFunctions.getVertices = gameId => {
-    return db.many(
-      "SELECT vertex_number,robber,player_id,building_type,port_id " +
-        'FROM "vertices" LEFT JOIN "buildings" ' +
-        'ON "vertices".building_id = "buildings".id ' +
-        "WHERE game_id = $1",
-      [gameId]
-    );
-  };
-
-  gameFunctions.getRoads = gameId => {
-    return db.many(
-      "SELECT  player_id,v_start.vertex_number vertex_start,v_end.vertex_number vertex_end" +
-        ' FROM "connections"' +
-        'INNER JOIN "vertices" v_start ON (v_start.id = "connections".vertex_start)' +
-        'INNER JOIN "vertices" v_end ON (v_end.id = "connections".vertex_end)' +
-        'WHERE "connections".game_id = $1',
-      [gameId]
-    );
-  };
   return gameFunctions;
 };
